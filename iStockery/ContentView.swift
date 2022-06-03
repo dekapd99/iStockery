56419fb60122be60232c541e72411b769da10f1f
dekapd99 <dekaprimatio@gmail.com>
2022-06-03 20:50:40 +0700

    Initial Commit

---
+++ iStockery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iStockery
+//
+//  Created by Deka Primatio on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iStockery/iStockeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStockeryApp.swift
+//  iStockery
+//
+//  Created by Deka Primatio on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iStockeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
